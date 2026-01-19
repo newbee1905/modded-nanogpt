18eca0ac4cf504a20624ecb47fa9384b9033b177
newbee1905 <beenewminh@outlook.com>
2026-01-19 21:08:11 +1100

    feat/wip: add hyper ball norm for qk norm #8

--- train_gpt.py
+++ train_gpt.py
@@ -1045,6 +1045,9 @@ def norm(x: torch.Tensor):
     
     return y_flat.view(input_shape)
 
+def norm(x: Tensor):
+    return F.rms_norm(x, (x.size(-1),))
+
 class CastedLinear(nn.Linear):
     def __init__(self, in_features: int, out_features: int, use_fp8=False, x_s=1.0, w_s=1.0, grad_s=1.0):
         super().__init__(in_features, out_features, bias=False)
